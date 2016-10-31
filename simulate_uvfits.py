c05d008275d9a95660a06b2031da08a486830ea7
Jack Line <jack.l.b.line@gmail.com>
2016-10-31 13:02:00 +1100

    Forgot to add simulate_uvfits.py in last commit

--- simulate_uvfits.py
+++ simulate_uvfits.py
@@ -95,6 +95,9 @@ parser.add_option('-c', '--tag_name',
 parser.add_option('-a', '--time_decor', default=False, action='store_true',
 	help='Add to include time_decorrelation in the simulation')
 
+parser.add_option('-z', '--freq_decor', default=False, action='store_true',
+	help='Add to include freq decorrelation in the simulation')
+
 parser.add_option('-b', '--diffuse', default=False, action='store_true',
 	help='Add to include the 2016 gsm sky model')
 
@@ -106,6 +109,9 @@ parser.add_option('-j', '--telescope', default='MWA_phase1',
 
 parser.add_option('-i', '--data_loc', default='./data',
 	help='Location to output the uvfits to OR location of uvfits if just adding diffuse model. Default = ./data')
+
+parser.add_option('-p', '--phase_centre', default=False,
+	help='Phase centre of the observation in degrees as RA,DEC - as a default tracks the intial zenith point')
 
 options, args = parser.parse_args()
 
@@ -121,6 +127,7 @@ srclist = options.srclist
 intial_date = options.date
 tag_name = options.tag_name
 time_decor = options.time_decor
+freq_decor = options.freq_decor
 
 data_loc = options.data_loc
 
@@ -139,10 +146,13 @@ MRO.date = '/'.join(date.split('-'))+' '+time
 intial_lst = float(MRO.sidereal_time())*R2D
 #intial_ra_point = 356.303842095
 intial_ra_point = float(MRO.sidereal_time())*R2D
+#print intial_ra_point
 dec_point = MWA_LAT
 
-
-
+if options.phase_centre:
+	ra_phase, dec_phase = map(float,options.phase_centre.split(','))
+else:
+	ra_phase, dec_phase = intial_ra_point, dec_point
 
 ##Get the local topocentric X,Y,Z values for the MWA using the local topocentric e,n,h
 ##values from the antenna locs in MWA_Tools
@@ -152,7 +162,7 @@ array_layout = "%s/telescopes/%s/antenna_locations_%s.txt" %(MAJICK_DIR,options.
 anntenna_locs = loadtxt(array_layout)
 X,Y,Z = enh2xyz(anntenna_locs[:,0],anntenna_locs[:,1],anntenna_locs[:,2],MWA_LAT*D2R)
 
-##TODO - migrate to new beam
+##TODO - migrate to new beam - but to do that, need a smooth frequency beam response
 ##TODO - get the delays in a smart way
 ##TODO - check whether the desired pointing exists in a smart way
 ##       when using the MWA beam as a gridding function
@@ -230,15 +240,18 @@ for freq in freq_range:
 		##Add in half a time resolution step to give the central LST
 		sky_offset = (((time + (time_res / 2.0))*SOLAR2SIDEREAL)*(15.0/3600.0))
 
+		##Currently always point to zenith
 		ra_point = intial_ra_point + sky_offset
 		if ra_point >=360.0: ra_point -= 360.0
 		lst = intial_lst + sky_offset
 		if lst >=360.0: lst -= 360.0
 		
 		ha_point = lst - ra_point
-		#print(ha_point)
+		ha_phase = lst - ra_phase
 		
-		this_date = add_time(intial_date,time + (time_res / 2.0))
+		##DO NOT ADD HALF A TIME RESOLUTION
+		##make sure this all happens when reading in the uvfits
+		this_date = add_time(intial_date,time)# + (time_res / 2.0))
 		
 		if srclist:
 			##TODO Weight each source by the beam pattern - do this to
@@ -250,21 +263,20 @@ for freq in freq_range:
 				else:
 					weight_by_beam(source=source,freqcent=freq_cent,LST=lst,beam=False)
 				
-			##Set out instrument to always point at the same HA, DEC for now
-			##TODO make an option to track an RA, DEC
-			base_header['CRVAL6'] = ra_point
-			base_header['CRVAL7'] = dec_point
-			base_header['CRVAL4'] = freq_cent
+			base_header['CRVAL6'] = ra_phase
+			base_header['CRVAL7'] = dec_phase
+			#base_header['CRVAL6'] = ra_point
+			#base_header['CRVAL7'] = dec_point
+			base_header['CRVAL4'] = freq * 1e+6
 			
 			##Change the date by the time step - this is used by MAJICK
 			##to get the LST
-			
 			antenna_header['RDATE'] = this_date
 			
 		#print 'srclist has been weighted by freq and beam'
-		
 		##GSM image and uv_data_array are the same for all baselines, for each time and freq
 		if options.diffuse:
+			##TODO - I think this may be half a time step off, need to give the central time
 			image, l_reso = generate_gsm_2016(freq=freq_cent,this_date=this_date,observer=MRO)
 			uv_data_array, u_sim, v_sim, u_reso = convert_image_lm2uv(image=image,l_reso=l_reso)
 			
@@ -275,12 +287,52 @@ for freq in freq_range:
 			#print 'Simulating baseline %04d' %baseline
 		#for baseline in range(0,1):
 			x_length,y_length,z_length = xyz_lengths[baseline]
-			u,v,w = get_uvw(x_length,y_length,z_length,dec_point*D2R,ha_point*D2R)
+			##The old way of non-phase tracking
+			#u,v,w = get_uvw(x_length,y_length,z_length,dec_point*D2R,ha_point*D2R)
+			u,v,w = get_uvw(x_length,y_length,z_length,dec_phase*D2R,ha_phase*D2R)
 			if srclist:
 				#print 'Adding point sources'
 				uv_data_XX = array([0.0,0.0,1.0])
 				uv_data_YY = array([0.0,0.0,1.0])
 				
+				##OLD non-phase tracking case
+				###For every source in the sky
+				#for name,source in sources.iteritems():
+					###If source is below the horizon, forget about it
+					#if source.skip:
+						#pass
+					###Otherwise, proceed
+					#else:
+						#if time_decor and freq_decor:
+							#if options.beam:
+								#model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,
+											#x_length=x_length,y_length=y_length,z_length=z_length,freq_decor=freq_res*1e+6,freq=freq_cent,time_decor=time_res,beam=True)
+							#else:
+								#model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,
+											#x_length=x_length,y_length=y_length,z_length=z_length,freq_decor=freq_res*1e+6,freq=freq_cent,time_decor=time_res)
+						#elif time_decor:
+							#if options.beam:
+								#model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,
+											#x_length=x_length,y_length=y_length,z_length=z_length,time_decor=time_res,beam=True)
+							#else:
+								#model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,
+											#x_length=x_length,y_length=y_length,z_length=z_length,time_decor=time_res)
+						#elif freq_decor:
+							#if options.beam:
+								#model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,
+											#x_length=x_length,y_length=y_length,z_length=z_length,freq_decor=freq_res*1e+6,freq=freq_cent,beam=True)
+							#else:
+								#model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,
+											#x_length=x_length,y_length=y_length,z_length=z_length,freq_decor=freq_res*1e+6,freq=freq_cent)
+						#else:
+							#if options.beam:
+								#model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,beam=True)
+							#else:
+								#model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst)
+							
+						#uv_data_XX += array([real(model_xxpol),imag(model_xxpol),0.0000])
+						#uv_data_YY += array([real(model_yypol),imag(model_yypol),0.0000])
+						
 				##For every source in the sky
 				for name,source in sources.iteritems():
 					##If source is below the horizon, forget about it
@@ -288,16 +340,32 @@ for freq in freq_range:
 						pass
 					##Otherwise, proceed
 					else:
-						if time_decor:
+						if time_decor and freq_decor:
 							if options.beam:
-								model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,x_length=x_length,y_length=y_length,z_length=z_length,time_decor=time_res,beam=True)
+								model_xxpol,model_yypol = model_vis_phasetrack(u=u,v=v,w=w,source=source,phase_ra=ra_phase,phase_dec=dec_phase,LST=lst,
+											x_length=x_length,y_length=y_length,z_length=z_length,freq_decor=freq_res*1e+6,freq=freq_cent,time_decor=time_res,beam=True)
 							else:
-								model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,x_length=x_length,y_length=y_length,z_length=z_length,time_decor=time_res)
+								model_xxpol,model_yypol = model_vis_phasetrack(u=u,v=v,w=w,source=source,phase_ra=ra_phase,phase_dec=dec_phase,LST=lst,
+											x_length=x_length,y_length=y_length,z_length=z_length,freq_decor=freq_res*1e+6,freq=freq_cent,time_decor=time_res)
+						elif time_decor:
+							if options.beam:
+								model_xxpol,model_yypol = model_vis_phasetrack(u=u,v=v,w=w,source=source,phase_ra=ra_phase,phase_dec=dec_phase,LST=lst,
+											x_length=x_length,y_length=y_length,z_length=z_length,time_decor=time_res,beam=True)
+							else:
+								model_xxpol,model_yypol = model_vis_phasetrack(u=u,v=v,w=w,source=source,phase_ra=ra_phase,phase_dec=dec_phase,LST=lst,
+											x_length=x_length,y_length=y_length,z_length=z_length,time_decor=time_res)
+						elif freq_decor:
+							if options.beam:
+								model_xxpol,model_yypol = model_vis_phasetrack(u=u,v=v,w=w,source=source,phase_ra=ra_phase,phase_dec=dec_phase,LST=lst,
+											x_length=x_length,y_length=y_length,z_length=z_length,freq_decor=freq_res*1e+6,freq=freq_cent,beam=True)
+							else:
+								model_xxpol,model_yypol = model_vis_phasetrack(u=u,v=v,w=w,source=source,phase_ra=ra_phase,phase_dec=dec_phase,LST=lst,
+											x_length=x_length,y_length=y_length,z_length=z_length,freq_decor=freq_res*1e+6,freq=freq_cent)
 						else:
 							if options.beam:
-								model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst,beam=True)
+								model_xxpol,model_yypol = model_vis_phasetrack(u=u,v=v,w=w,source=source,phase_ra=ra_phase,phase_dec=dec_phase,LST=lst,beam=True)
 							else:
-								model_xxpol,model_yypol = model_vis(u=u,v=v,w=w,source=source,phase_ra=ra_point,phase_dec=dec_point,LST=lst)
+								model_xxpol,model_yypol = model_vis_phasetrack(u=u,v=v,w=w,source=source,phase_ra=ra_phase,phase_dec=dec_phase,LST=lst)
 							
 						uv_data_XX += array([real(model_xxpol),imag(model_xxpol),0.0000])
 						uv_data_YY += array([real(model_yypol),imag(model_yypol),0.0000])
@@ -307,6 +375,13 @@ for freq in freq_range:
 				#uv_data_YY[2] = 1.0
 				base_data[baseline][5][0,0,0,0,0,:] = uv_data_XX
 				base_data[baseline][5][0,0,0,0,1,:] = uv_data_YY
+				base_data[baseline][5][0,0,0,0,2,:] = array([0.0,0.0,1.0])
+				base_data[baseline][5][0,0,0,0,3,:] = array([0.0,0.0,1.0])
+				
+				#print base_data[baseline][5][0,0,0,0,0,:]
+				#print base_data[baseline][5][0,0,0,0,1,:]
+				#print "---------------------"
+				
 				base_data[baseline][0] = u / freq_cent
 				base_data[baseline][1] = v / freq_cent
 				base_data[baseline][2] = w / freq_cent
@@ -329,23 +404,23 @@ for freq in freq_range:
 					if outside: outside_uv += 1
 					
 					#l,m,n = get_lm(ra_point*D2R,ra_phase*D2R, MWA_LAT*D2R, MWA_LAT*D2R)
-					uv_complex_XX = reverse_grid(uv_data_array=uv_data_array, l_reso=l_reso, u=u, v=v, kernel=options.telescope,freq_cent=freq_cent,u_reso=u_reso,u_sim=u_sim,v_sim=v_sim)
+					uv_complex_XX,uv_complex_YY = reverse_grid(uv_data_array=uv_data_array, l_reso=l_reso, u=u, v=v, kernel=options.telescope,freq_cent=freq_cent,u_reso=u_reso,u_sim=u_sim,v_sim=v_sim)
 					PhaseConst = 1j * 2 * pi
 					##Insert a w-term as the FFT doesn't include them??
 					##Inserting the w for zenith pointing where n=1
+					
+					##TODO - is this phase tracking case? Or add in an option for phase tracking??
 					n = 1
 					uv_complex_XX *= exp(PhaseConst * w*n)
 					
 					base_data[baseline][5][0,0,0,0,0,:] += array([real(uv_complex_XX),imag(uv_complex_XX),0.0000])
-					#uv_data_YY = base_data[baseline][5][0,0,0,0,1,:]
+					base_data[baseline][5][0,0,0,0,1,:] += array([real(uv_complex_YY),imag(uv_complex_YY),0.0000])
 				except:
 					skipped_gsm += 1
-					
+				
 		if options.diffuse:
 			print '%04d out of %04d baselines skipped in gsm, u,v point outside gsm uv data plane' %(skipped_gsm,len(base_data))
-		#print '%04d out of %04d baselines defo outside uv data plane' %(outside_uv,len(base_data))
 				
-			
 		if time_res < 1:
 			uvfits_name = "%s_%.3f_%05.2f.uvfits" %(tag_name,freq,time)
 		else:
